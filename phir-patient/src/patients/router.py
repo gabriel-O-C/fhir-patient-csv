240ca43f10374944b8a678b27a956d230b4af023
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2024-09-07 18:21:48 -0300

    refactor: move csv parsing for its  own function

--- phir-patient/src/patients/router.py
+++ phir-patient/src/patients/router.py
@@ -6,6 +6,26 @@ from io import StringIO
 router = APIRouter(prefix="/api/v1", tags=["patients"])
 
 
+def parse_csv(file: UploadFile):
+    if file.content_type != "text/csv":
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST, detail="Invalid file type"
+        )
+    try:
+        contents = file.file.read()
+        try:
+            decoded_content = contents.decode("utf-8")
+        except UnicodeDecodeError:
+            decoded_content = contents.decode("latin-1")
+        csv_data = StringIO(decoded_content)
+        csv_reader = csv.DictReader(csv_data)
+        patients_list = [row for row in csv_reader]
+        return patients_list
+
+    except Exception as e:
+        raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail=str(e))
+
+
 @router.post("/patients", status_code=HTTPStatus.CREATED)
 async def create_patients(file: UploadFile = File(...)):
     if file.content_type != "text/csv":
@@ -13,18 +33,6 @@ async def create_patients(file: UploadFile = File(...)):
             status_code=HTTPStatus.BAD_REQUEST, detail="Invalid file type"
         )
 
-    try:
-        contents = file.file.read()
-        try:
-            decoded_content = contents.decode("utf-8")
-        except UnicodeDecodeError:
-            decoded_content = contents.decode("latin-1")
+    patients_list = parse_csv(file)
 
-        csv_data = StringIO(decoded_content)
-        csv_reader = csv.DictReader(csv_data)
-
-        patients_list = [row for row in csv_reader]
-        return {"message": "Successfully created patients", "patients": patients_list}
-
-    except Exception as e:
-        raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail=str(e))
+    return {"message": "Successfully created patients", "patients": patients_list}
