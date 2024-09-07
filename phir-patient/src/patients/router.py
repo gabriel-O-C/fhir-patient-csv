0c4b3b2e0fa54f49130f38c1ae3209deb4cbfef2
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2024-09-07 19:12:27 -0300

    style: lint

--- phir-patient/src/patients/router.py
+++ phir-patient/src/patients/router.py
@@ -1,38 +1,44 @@
-from fastapi import APIRouter, HTTPException, UploadFile, File
-from http import HTTPStatus
 import csv
+from http import HTTPStatus
 from io import StringIO
 
-router = APIRouter(prefix="/api/v1", tags=["patients"])
+from fastapi import APIRouter, File, HTTPException, UploadFile
+
+router = APIRouter(prefix='/api/v1', tags=['patients'])
 
 
 def parse_csv(file: UploadFile):
-    if file.content_type != "text/csv":
+    if file.content_type != 'text/csv':
         raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST, detail="Invalid file type"
+            status_code=HTTPStatus.BAD_REQUEST, detail='Invalid file type'
         )
     try:
         contents = file.file.read()
         try:
-            decoded_content = contents.decode("utf-8")
+            decoded_content = contents.decode('utf-8')
         except UnicodeDecodeError:
-            decoded_content = contents.decode("latin-1")
+            decoded_content = contents.decode('latin-1')
         csv_data = StringIO(decoded_content)
         csv_reader = csv.DictReader(csv_data)
         patients_list = [row for row in csv_reader]
         return patients_list
 
     except Exception as e:
-        raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail=str(e))
-
-
-@router.post("/patients", status_code=HTTPStatus.CREATED)
-async def create_patients(file: UploadFile = File(...)):
-    if file.content_type != "text/csv":
         raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST, detail="Invalid file type"
+            status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail=str(e)
+        )
+
+
+@router.post('/patients', status_code=HTTPStatus.CREATED)
+def create_patients(file: UploadFile = File(...)):
+    if file.content_type != 'text/csv':
+        raise HTTPException(
+            status_code=HTTPStatus.BAD_REQUEST, detail='Invalid file type'
         )
 
     patients_list = parse_csv(file)
 
-    return {"message": "Successfully created patients", "patients": patients_list}
+    return {
+        'message': 'Successfully created patients',
+        'patients': patients_list,
+    }
